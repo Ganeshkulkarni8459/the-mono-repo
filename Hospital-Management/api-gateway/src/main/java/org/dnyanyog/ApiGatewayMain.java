2cb8572d644217e7e82b1997e1460cca19a22baf
Ganesh Kulkarni <gkulkarni@dnyanyog.org>
2024-08-21 18:56:44 +0530

    ci: Fix Bugs

--- Hospital-Management/api-gateway/src/main/java/org/dnyanyog/ApiGatewayMain.java
+++ Hospital-Management/api-gateway/src/main/java/org/dnyanyog/ApiGatewayMain.java
@@ -3,7 +3,6 @@ package org.dnyanyog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ApiGatewayMain {
 
